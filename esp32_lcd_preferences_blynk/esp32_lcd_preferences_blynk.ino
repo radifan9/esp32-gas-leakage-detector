349d30631bcb54801309295a5368be588ef95c7a
Radif Aghnadiin <radif.aghnadiin@gmail.com>
2023-06-03 01:12:04 +0700

    updated.

--- esp32_lcd_preferences_blynk/esp32_lcd_preferences_blynk.ino
+++ esp32_lcd_preferences_blynk/esp32_lcd_preferences_blynk.ino
@@ -1,7 +1,12 @@
-// Pin Setup
+// Pin & General Setup
 int i2c_tr = 6;
 int mq2_tr = 7;
 int buzzer_tr = 10;
+unsigned int counter;
+
+// Watchdog Timer
+#include <esp_task_wdt.h>
+#define WDT_TIMEOUT 10     // 6 seconds WDT
 
 // Setup for LCD & KEYPAD
 #include <Wire.h>
@@ -59,15 +64,79 @@ enum State {
   ENTER_SSID,
   ENTER_PASSWORD,
   AUTHENTICATE,
-  ACCESS_GRANTED,
-  ACCESS_DENIED,
   BLYNK_RUN
 };
 
-State currentState = ENTER_SSID; // Initial state
+// State currentState = ENTER_SSID; // Initial state
+State currentState = AUTHENTICATE; // Initial state
+
+void reset_counter() {
+  // Configuring flash memory
+  preferences.begin("wifi_retry", false);
+  counter = preferences.getUInt("counter", 0);
+  Serial.println();
+  Serial.printf("wifi-retry counter value: %u\n", counter);
+  Serial.printf("-- wifi-retry counter reseted. --");
+  counter = 0;
+  preferences.putUInt("counter", counter);    // Store the counter to the Preferences
+  preferences.end();  
+}
+
+#include "pitches.h"  // must include open source pitches.h found online in libraries folder
+void click_sound() {
+  // digitalWrite(buzzer_tr, HIGH);
+  // delay(10);
+  // digitalWrite(buzzer_tr, LOW);
+  tone(buzzer_tr,NOTE_G4,35);
+  delay(35);
+  tone(buzzer_tr,NOTE_G5,35);
+  delay(35);
+  tone(buzzer_tr,NOTE_G6,35);
+  delay(35);
+  noTone(buzzer_tr);
+}
+
+void congrats_sound() {
+  // digitalWrite(buzzer_tr, HIGH);
+  // delay(50);
+  // digitalWrite(buzzer_tr, LOW);
+  // tone(buzzer_tr,NOTE_E6,125);
+  // delay(130);
+  // tone(buzzer_tr,NOTE_G6,125);
+  // delay(130);
+  // tone(buzzer_tr,NOTE_E7,125);
+  // delay(130);
+  // tone(buzzer_tr,NOTE_C7,125);
+  // delay(130);
+  // tone(buzzer_tr,NOTE_D7,125);
+  // delay(130);
+  // tone(buzzer_tr,NOTE_G7,125);
+  // delay(125);
+  // noTone(buzzer_tr);
+
+    tone(buzzer_tr, 523.25, 133);
+    delay(133);
+    tone(buzzer_tr, 523.25, 133);
+    delay(133);
+    tone(buzzer_tr, 523.25, 133);
+    delay(133);
+    tone(buzzer_tr, 523.25, 400);
+    delay(400);
+    tone(buzzer_tr, 415.30, 400);
+    delay(400);
+    tone(buzzer_tr, 466.16, 400);
+    delay(400);
+    tone(buzzer_tr, 523.25, 133);
+    delay(133);
+    delay(133);
+    tone(buzzer_tr, 466.16, 133);
+    delay(133);
+    tone(buzzer_tr, 523.25, 1200);
+    delay(1200);
+}
 
 void setup() {
-  // Pin Setup
+  // Hardware & Pin Setup
   pinMode(i2c_tr, OUTPUT);
   digitalWrite(i2c_tr, HIGH);
   pinMode(mq2_tr, OUTPUT);
@@ -75,16 +144,41 @@ void setup() {
   pinMode(buzzer_tr, OUTPUT);
   digitalWrite(buzzer_tr, LOW);
 
-  // Configuring Hardware
   Serial.begin(115200);
 
   // Configuring I2C LCD and Keypad
   Wire.begin();          // Call the connection Wire
   keypad.begin();        // Call the connection for the keypad
+
   lcd.begin(16, 2);      // Initialize the LCD with 16 columns and 2 rows
   lcd.backlight();       // Turn on the LCD backlight
-  lcd.print("Enter SSID:"); // Display initial message on the LCD
+  lcd.print("Embedded System");
+  lcd.setCursor(0, 1);
+  lcd.print("      (B)");
 
+
+  // Configuring flash memory
+  preferences.begin("wifi_retry", false);
+  counter = preferences.getUInt("counter", 0);
+  Serial.println();
+  Serial.printf("wifi-retry counter value: %u\n", counter);
+  counter++;
+  preferences.putUInt("counter", counter);    // Store the counter to the Preferences
+  preferences.end();                          // Close the Preferences
+  // counter = 0;
+  // reset_counter();
+
+  // Enable Watchdog timer
+  delay(2000);
+  esp_task_wdt_init(WDT_TIMEOUT, true);
+  esp_task_wdt_add(NULL);
+
+
+  if (counter >= 3) {
+    lcd.clear();
+    lcd.print("Enter SSID:");
+    currentState = ENTER_SSID;
+  }
 }
 
 void loop() {
@@ -95,10 +189,13 @@ void loop() {
     case ENTER_SSID:
       if (key != NO_KEY) {
         if (key == '#') {
+          click_sound();
           lcd.clear();
           lcd.print("Enter Password:");
           currentState = ENTER_PASSWORD;
         } else {
+          click_sound();
+          esp_task_wdt_reset(); // Resetting Timer every key pressed
           ssid += key;
           lcd.setCursor(ssid.length() - 1, 1);
           lcd.print(key);
@@ -109,11 +206,14 @@ void loop() {
     case ENTER_PASSWORD:
       if (key != NO_KEY) {
         if (key == '#') {
+          click_sound();
           lcd.clear();
           lcd.print("Authenticating...");
 
-          ssid.toCharArray(ssid_char, sizeof(ssid_char));
-          password.toCharArray(pass_char, sizeof(pass_char));
+          preferences.begin("credentials", false);
+          preferences.putString("ssid", ssid);
+          preferences.putString("password", password);
+          preferences.end();
 
           currentState = AUTHENTICATE;
         } else if (key == '*') {
@@ -122,6 +222,8 @@ void loop() {
           lcd.print("Enter SSID:");
           currentState = ENTER_SSID;
         } else {
+          click_sound();
+          esp_task_wdt_reset(); // Resetting Timer every key pressed
           password += key;
           lcd.setCursor(password.length() - 1, 1);
           lcd.print(key); // Display the entered character on the LCD
@@ -130,119 +232,73 @@ void loop() {
       break;
 
     case AUTHENTICATE:
-      // Simulating authentication by comparing entered SSID and password against saved values
+      esp_task_wdt_reset();
+
+      lcd.clear();
+      lcd.print("Authenticating...");
+      
+      // Configuring flash memory
+      preferences.begin("credentials", false);
+      ssid = preferences.getString("ssid", "");
+      password = preferences.getString("password", "");
+      preferences.end(); 
+
+      ssid.toCharArray(ssid_char, sizeof(ssid_char));
+      password.toCharArray(pass_char, sizeof(pass_char));
 
       Blynk.begin(auth, ssid_char, pass_char);
 
       lcd.clear();
       lcd.print("Wifi Connected.");
 
+      congrats_sound();
+
+      esp_task_wdt_reset();
+
+      // preferences.begin("wifi_reset", false);
+      // // int counter_reset = 0;
+      // counter = 0;
+      // preferences.putUInt("counter", counter);
+      // // preferences.end();    // Close the Preferences
+
+      // Configuring flash memory
+      // preferences.begin("wifi_retry", false);
+      // // counter = preferences.getUInt("counter", 0);
+      // counter = 0;
+      // Serial.println();
+      // Serial.printf("wifi-retry counter value: %u\n", counter);
+      // preferences.putUInt("counter", counter);    // Store the counter to the Preferences
+      // preferences.end();                          // Close the Preferences
+
+      reset_counter();
+
       delay(2000);
 
-      currentState = BLYNK_RUN;
-      // if (ssid == "192168" && password == "1234") {
-      //   lcd.clear();
-      //   lcd.print("Access Granted");
-      //   currentState = ACCESS_GRANTED;
-
-      //   // Save SSID and password to EEPROM
-      //   saveToEEPROM();
-
-      //   // Print the EEPROM contents to Serial Monitor
-      //   printFromEEPROM();
-      // } else {
-      //   lcd.clear();
-      //   lcd.print("Access Denied");
-      //   currentState = ACCESS_DENIED;
-      // }
-
-
-
-      // delay(2000);
-      // lcd.clear();
-      // lcd.print("Enter SSID:");
-      // ssid = "";
-      // password = "";
-      // currentState = ENTER_SSID;
-      break;
-
-    case ACCESS_GRANTED:
-      // Access granted, do something here
-      break;
-
-    case ACCESS_DENIED:
-      // Access denied, do something here
-      break;
-
-    case BLYNK_RUN:
       lcd.clear();
       lcd.print("  Connected to");
       lcd.setCursor(0, 1);
       lcd.print("     Blynk.");
       delay(2000);
 
+      esp_task_wdt_reset();
+
       lcd.clear();
       lcd.print("  Turning off");
       lcd.setCursor(0, 1);
       lcd.print(" LCD and keypad.");
       delay(2000);
+      lcd.noBacklight();
+      delay(500);
+      // digitalWrite(i2c_tr, LOW);
 
-      digitalWrite(i2c_tr, LOW);
+      currentState = BLYNK_RUN;
+      break;
+
+    case BLYNK_RUN:
+      esp_task_wdt_reset();
+      
       Blynk.run();
       timer.run();
       break;
   }
-}
-
-void saveToEEPROM() {
-  // Calculate the total number of bytes needed to store the SSID and password
-  int ssidSize = ssid.length() + 1;     // Include null terminator
-  int passwordSize = password.length() + 1; // Include null terminator
-  int totalSize = ssidSize + passwordSize;
-
-  // Write the SSID to EEPROM
-  for (int i = 0; i < ssidSize; i++) {
-    EEPROM.write(i, ssid[i]);
-  }
-
-  // Write the password to EEPROM
-  for (int i = 0; i < passwordSize; i++) {
-    EEPROM.write(ssidSize + i, password[i]);
-  }
-
-  // Write the total size to EEPROM
-  EEPROM.write(0, totalSize);
-
-  // Commit the changes to EEPROM
-  EEPROM.commit();
-}
-
-void readFromEEPROM() {
-  // Read the total size from EEPROM
-  int totalSize = EEPROM.read(0);
-
-  // Read the SSID from EEPROM
-  for (int i = 0; i < totalSize; i++) {
-    char c = EEPROM.read(i);
-    if (c == '\0') {
-      break; // Null terminator reached, stop reading
-    }
-    ssid += c;
-  }
-
-  // Read the password from EEPROM
-  for (int i = totalSize; i < totalSize * 2; i++) {
-    char c = EEPROM.read(i);
-    if (c == '\0') {
-      break; // Null terminator reached, stop reading
-    }
-    password += c;
-  }
-}
-
-void printFromEEPROM() {
-  
-
-  Serial.println("Saved SSID: " + ssid);
-  Serial.println("Saved Password: " + password);
 }
